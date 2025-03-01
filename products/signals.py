7c11221f02400ca9ec3ccb994f202b98edbcdde8
Manu-080 <manusankardemo@gmail.com>
2025-03-01 09:03:23 +0530

    fix : fixed discount price calculation in signals

--- products/signals.py
+++ products/signals.py
@@ -7,5 +7,6 @@ from .models import Product
 @receiver(pre_save, sender=Product)
 def update_discount_price(sender, instance, **kwargs): # sender=Prduct , instace is the product object, and other keyword arguments.
     """Automatically calculate and update the discount price before saving."""
-    instance.discount_price = int(instance.price * (Decimal(1) - instance.discount/Decimal(100)))
+    if instance.discount is not None:
+        instance.discount = int(((instance.price - instance.discount_price) / instance.price) * Decimal(100))
     
