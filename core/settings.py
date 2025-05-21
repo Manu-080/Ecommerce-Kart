feaeaee4a14e122836607239c0ddadac3f393368
Manu-080 <manusankardemo@gmail.com>
2025-05-21 17:39:28 +0530

    feat : changed database for render

--- core/settings.py
+++ core/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
